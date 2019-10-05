135842b28450fef7d25a692cae1d53a460bd40f0
subires <dsubires@viewnext.com>
2019-10-05 11:22:24 +0200

    si no hay información, no publica mensajes vacíos

--- src/main/java/com/dsubires/lunchtime/ScheduledTaskService.java
+++ src/main/java/com/dsubires/lunchtime/ScheduledTaskService.java
@@ -30,8 +30,8 @@ import twitter4j.TwitterFactory;
 @Service
 public class ScheduledTaskService {
 
-	private static final String user = "user";
-	private static final String passwd = "passwd";
+	private static final String user = "";
+	private static final String passwd = "";
 	private static final String loginFormUrl = "https://timechef.serunion.com/account/login?ReturnUrl=/menus/hoy/7";
 	private static final String loginActionUrl = "https://timechef.serunion.com/account/login";
 	private static final String USER_AGENT = "\"Mozilla/5.0 (Windows NT\" +\n"
@@ -39,7 +39,7 @@ public class ScheduledTaskService {
 	private static String primeros, segundos, plancha, postres, guarniciones;
 	private Logger logger = LogManager.getLogger("informationDumping");
 
-	@Scheduled(cron = "0 45 9 * * MON-FRI")
+	@Scheduled(cron = "0 02 12 * * MON-FRI")
 	public void scheduleTask() {
 		
 		HashMap<String, String> cookies = new HashMap<String, String>();
@@ -53,6 +53,7 @@ public class ScheduledTaskService {
 		int mes = localDate.getMonthValue();
 		int dia = localDate.getDayOfMonth();
 		Connection.Response loginForm;
+		int elementosMenu = 0;
 		
 		try {
 			// 1. GET LOGIN FORM
@@ -87,27 +88,32 @@ public class ScheduledTaskService {
 			Document doc = Jsoup.parse(html);
 
 			Elements datos = doc.select("#acordeonmenuIngesta1 > ul > li:first-child > div > div.dish > p");
+			elementosMenu += datos.size();
 
 			primeros += "Primeros:\n";
 			for (Element element : datos)
 				primeros += WordUtils.capitalizeFully(element.text()) + "\n";
 
 			datos = doc.select("#acordeonmenuIngesta1 > ul > li:nth-child(2) > div > div.dish > p");
+			elementosMenu += datos.size();
 			segundos += "Segundos:\n";
 			for (Element element : datos)
 				segundos += WordUtils.capitalizeFully(element.text()) + "\n";
 
 			datos = doc.select("#acordeonmenuIngesta7 > ul > li p");
+			elementosMenu += datos.size();
 			guarniciones += "Guarniciones:\n";
 			for (Element element : datos)
 				guarniciones += WordUtils.capitalizeFully(element.text()) + "\n";
 
 			datos = doc.select("#acordeonmenuIngesta6 > ul > li p");
+			elementosMenu += datos.size();
 			plancha += "Plancha:\n";
 			for (Element element : datos)
 				plancha += WordUtils.capitalizeFully(element.text()) + "\n";
 
 			datos = doc.select("#acordeonmenuIngesta1 > ul > li:nth-child(3) > div > div.dish > p");
+			elementosMenu += datos.size();
 			postres += "Postres:\n";
 			for (Element element : datos)
 				postres += WordUtils.capitalizeFully(element.text()) + "\n";
@@ -119,15 +125,32 @@ public class ScheduledTaskService {
 			String msgTwitter1 = "Menú " + dia + "/" + mes + "\n" + primeros + "\n" + segundos + "\n" + guarniciones;
 			String msgTwitter2 = "Menú " + dia + "/" + mes + "\n" + plancha + "\n" + postres;
 
-			sendToTelegram(msgTelegram);
-			sendToTwitter(msgTwitter1);
-			sendToTwitter(msgTwitter2);
+			
+			if(elementosMenu == 0) {
+				logger.error("Error al publicar el menú. No se ha encontrado la información en la web de serunion.");
+			}else {
+				sendToTelegram(msgTelegram);
+				if(msgTwitter1.length() < 160 && msgTwitter2.length() < 160) {
+					sendToTwitter(msgTwitter1);
+					sendToTwitter(msgTwitter2);	
+				}else {
+					msgTwitter1 = "Menú " + dia + "/" + mes + "\n" + primeros + "\n" + segundos;
+					String msgTwitter3 = msgTwitter2;
+					msgTwitter2 = "Menú " + dia + "/" + mes + "\n" + guarniciones;
+					sendToTwitter(msgTwitter1);
+					sendToTwitter(msgTwitter2);
+					sendToTwitter(msgTwitter3);
+					
+				}
+				logger.info("Menú " + dia + "/" + mes + " publicado correctamente. " + new Date() );
+			}
+			
+			
 
-		} catch (Exception e) {
-			logger.info("Error al publicar el menú.\nFecha: " + new Date() + "\nInfo: " + e.getMessage() );
+		} catch (IOException e) {
+			logger.info("Error al obtener el menú.\nFecha: " + new Date() + "\nInfo: " + e.getMessage() );
 		}
 
-		logger.info("Menú " + dia + "/" + mes + " publicado correctamente. " + new Date() );
 		
 	}
 
@@ -146,10 +169,10 @@ public class ScheduledTaskService {
 		String urlString = "https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s";
 
 		// Add Telegram token (given Token is fake)
-		String apiToken = "800000000:AAGxLYq5czZ_asOkDsWJyKOddbiG6ig8Xt4";
+		String apiToken = "";
 
 		// Add chatId (given chatId is fake)
-		String chatId = "-1001400000000";
+		String chatId = "";
 
 		urlString = String.format(urlString, apiToken, chatId, encodeValue(str));
 
